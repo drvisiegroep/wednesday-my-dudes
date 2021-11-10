780fe68906582376e65aef52bf0538e1e465e302
daniel <daniel@websiteaanbieder.nl>
2021-11-10 11:44:26 +0100

    Fetch Api gemaakt

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,13 +1,26 @@
-
+import React, {useState, useEffect } from 'react'
 
 export function FetchUserData() {
-    fetch('https://jsonplaceholder.typicode.com/todos/')
-        .then(response => response.json())
-        .then(json => console.log(json))
-    
-        return(
+    const [resourceType, setResourceType] = useState('posts')
 
-           <h1>Harry Potter is great</h1>
-        )
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }, [resourceType])
+
+    return (
+        <>
+        <div>
+            <button onClick={() => setResourceType('posts')}>Posts</button>
+            <button onClick={() => setResourceType('users')}>Users</button>
+            <button onClick={() => setResourceType('comments')}>Comments</button>
+            <button onClick={() => setResourceType('todos')}>Todos</button>
+            <button onClick={() => setResourceType('photos')}>Photos</button>
+        </div>
+        <h1>{resourceType}</h1>
+        </>
+    )
 }
    
