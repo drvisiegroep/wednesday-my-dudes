dc5cd60523e6c8d46d2bf229b0a6d93ec19c82e7
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-12 20:25:32 +0100

    EXPANDED USER DATA

--- src/Users.js
+++ src/Users.js
@@ -1,15 +1,37 @@
 const Users = ({ results }) => {
+
     return (
 
         <div className="user-list">
 
         {results.map(result => (
-            <div className="user" key={result.id} >
-                <h2>Name: {result.name}</h2>
-                <p>Phone: {result.phone}</p>
-            </div>
+            <ul className="user" key={result.id} >
+                <h2>{result.name}</h2>
+                <li>Username: {result.username}</li>
+                <li>Email: {result.email}</li>
+                <li>lihone: {result.lihone}</li>
+                <li>Website: {result.website}</li>
+                    <ul>
+                        <li>Street: {result.address.street}</li>
+                        <li>Suite: {result.address.suite}</li>
+                        <li>City: {result.address.city}</li>
+                        <li>Zipcode: {result.address.zipcode}</li>
+                        <ul>
+                            <li>{result.address.geo.lat}</li>
+                            <li>{result.address.geo.lng}</li>
+                        </ul>
+                    </ul>
+                    <ul>
+                        <li>Company name: {result.company.name}</li>
+                        <li>Slogan: {result.company.catchPhrase}</li>
+                        <li>Business: {result.company.bs}</li>
+                    </ul>
+            </ul>
         ))}
 
+        <button>Previous</button>
+        <button>Next</button>
+        
         </div>
 
     );
