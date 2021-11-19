a95a9737a38cea8bf683192bcaebefdba833c265
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-19 20:55:59 +0100

    fetch uitgebreid en bestand opgeschoond

--- src/Fetch.js
+++ src/Fetch.js
@@ -6,14 +6,21 @@ import Presence from './Presence';
 export function FetchUserData() {
     const [resourceType, setResourceType] = useState('schedule');
     const [result, setResult] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        
+        setLoading(true)
         fetch(`https://aanmelden.visie-groep.nl/${resourceType}.php`)
         .then(response => response.json())
         .then(response => setResult(response))
-
+        .catch(err => console.log(err))
+        .finally(setLoading(false))
     }, [resourceType])
+
+    if (loading) {
+        console.log(`loading...`)
+        return <p>De data wordt geladen...</p>;
+    }
 
     return (
 
@@ -22,7 +29,6 @@ export function FetchUserData() {
             <div>
                 <button onClick={() => setResourceType('presence')}>Presence</button>
                 <button onClick={() => setResourceType('schedule')}>Schedule</button>
-
             </div>
             
             <h1>{resourceType}</h1>
