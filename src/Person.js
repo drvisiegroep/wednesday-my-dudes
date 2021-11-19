f9858b5495f6f74f82d51c455e570df27bfbcea5
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-19 20:55:15 +0100

    inline checks voor nulldata en schoonheidsfoutjes

--- src/Person.js
+++ src/Person.js
@@ -16,57 +16,60 @@ function Person({ userid }) {
             
     }, [userid]);
 
+    // Checken of de data is geladen vanuit de api.
     if (loading) {
-        console.log(`loading`)
-        return <p>Data is loading...</p>;
+        console.log(`loading...`)
+        return <p>De data wordt geladen...</p>;
       }
-    if(typeof person.schedule === 'undefined') {
-        console.log(`Undefined`)
-        return <p>Problem collecting the data (Undefined)</p>
+
+    // Checken of de json klaar is voor gebruik. Checken op alleen person werkt niet, blijkbaar is de dom dan nog niet geladen o.i.d.
+    // Betere oplossing voor zoeken, maar dit werkt voor nu.
+    if(typeof person.schedule === 'undefined' || typeof person.registrations === 'undefined') {
+        console.log(`Undefined...`)
+        return <p>Er was een foutje met het ophalen van de data...</p>
     }
     return (
 
         <>
-            
-                    <div>
-                        
-                        <h1>{person.first_name} {person.last_name}</h1>
-                        <p>Geboortedatum: {person.date_of_birth}</p>
-                        <p>Rol: {person.role}</p>
-                        <p>Op tijd: {person.arrive_on_time_target}</p>
-                        <p>Medische informatie: {person.medical_info}</p>
-                        <p>Begeleider: {person.personal_coach}</p>
-                        
-
-                        <h2>Schedule</h2>
-                        {person.schedule.map((schdl, schdlkey) => {
-                            return(
-                                <div key={schdlkey}>
-                                    <p>Afedling: {schdl.department}</p>
-                                    <p>Maandag {schdl.monday_from} tot {schdl.monday_to}</p>
-                                    <p>Dinsdag: {schdl.tuesday_from} tot {schdl.tuesday_to}</p>
-                                    <p>Woensdag: {schdl.wednesday_from} tot {schdl.wednesday_to}</p>
-                                    <p>Donderdag: {schdl.thursday_from} tot {schdl.thursday_to}</p>
-                                    <p>Vrijdag: {schdl.friday_from} tot {schdl.friday_to}</p>
-                                </div>
-                            )
-                        })}
-
-
-                        <h2>Aanmeldingen</h2>
-                        {person.registrations.map((reg, regkey) => {
-                           
-                           return(
-                               <div key={regkey}>
-                                <p>Signed in: {reg.signed_in}</p>
-                                <p>Signed off: {reg.signed_off}</p>
-                                <p>Automatisch: {reg.signed_auto_signed_off}</p>
-                                <p>---</p>
-                               </div>
-                           )
-                        })}
-                   </div>
+            <div className="dossier">
                 
+                <h1>Algemene informatie</h1>
+                <h2>{person.first_name} {person.last_name}</h2>
+                <p>Geboortedatum: {person.date_of_birth}</p>
+                <p>Rol: {person.role}</p>
+                <p>Op tijd: {person.arrive_on_time_target}</p>
+                <p>Medische informatie: {person.medical_info}</p>
+                <p>Begeleider: {person.personal_coach}</p>
+                
+
+                <h2>Schedule</h2>
+                {person.schedule.map((schdl, schdlkey) => {
+                    return(
+                        <div key={schdlkey}>
+                            <p>Afdeling: {schdl.department}</p>
+                            {schdl.monday_from === null ? <p>Maandag: Niet aanwezig</p> : <p>Maandag {schdl.monday_from} tot {schdl.monday_to}</p>}
+                            {schdl.tuesday_from === null ? <p>Dinsdag: Niet aanwezig</p> : <p>Dinsdag {schdl.tuesday_from} tot {schdl.tuesday_to}</p>}
+                            {schdl.wednesday_from === null ? <p>Woensdag: Niet aanwezig</p> : <p>Woensdag {schdl.wednesday_from} tot {schdl.wednesday_to}</p>}
+                            {schdl.thursday_from === null ? <p>Donderdag: Niet aanwezig</p> : <p>Donderdag {schdl.thursday_from} tot {schdl.thursday_to}</p>}
+                            {schdl.friday_from === null ? <p>Vrijdag: Niet aanwezig</p> : <p>Vrijdag {schdl.friday_from} tot {schdl.friday_to}</p>}
+                        </div>
+                    )
+                })}
+
+
+                <h2>Aanmeldingen</h2>
+                {person.registrations.map((reg, regkey) => {
+                    return(
+                        <div key={regkey}>
+                        <p>Signed in: {reg.signed_in}</p>
+                        <p>Signed off: {reg.signed_off}</p>
+                        <p>Automatisch: {reg.signed_auto_signed_off}</p>
+                        <p>---</p>
+                        </div>
+                    )
+                })}
+
+            </div>
         </>
     )
     
