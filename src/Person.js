8fd11c642cc95d0adb50634af34d5d6bf91b637f
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-19 19:00:31 +0100

    Undefined bug gefixed dus we kunnen eindelijk gaan mappen door data
    Vet chill

--- src/Person.js
+++ src/Person.js
@@ -23,8 +23,8 @@ function Person({ userid }) {
         return <p>Data is loading...</p>;
       }
     if(typeof person.schedule === 'undefined') {
-        console.log(`Shit's undefined, yo!`)
-        return <p> Shit's undefined, yo!</p>
+        console.log(`Undefined`)
+        return <p>Problem collecting the data (Undefined)</p>
     }
     return (
 
@@ -33,25 +33,44 @@ function Person({ userid }) {
         <>
             
                 
-                    {/* ({(person && typeof person.schedule !== "undefined")} */}
+                    {/* {(person && typeof person.schedule !== "undefined")} */}
                     <div>
+                        
                         <h1>{person.first_name} {person['last_name']}</h1>
                         <p>Geboortedatum: {person.date_of_birth}</p>
                         <p>Rol: {person.role}</p>
                         <p>Op tijd: {person.arrive_on_time_target}</p>
                         <p>Medische informatie: {person.medical_info}</p>
                         <p>Begeleider: {person.personal_coach}</p>
-                        <p>---</p>
-                        
-                        <p>Afdeling: {person['schedule']['0']['department']}</p>
-                        <p>Maandag {person.schedule[0].monday_from} tot {person.schedule[0].monday_to}</p>
-                        <p>Dinsdag: {person.schedule[0].tuesday_from} tot {person.schedule[0].tuesday_to}</p>
-                        <p>Woensdag: {person.schedule[0].wednesday_from} tot {person.schedule[0].wednesday_to}</p>
-                        <p>Donderdag: {person.schedule[0].thursday_from} tot {person.schedule[0].thursday_to}</p>
-                        <p>Vrijdag: {person.schedule[0].friday_from} tot {person.schedule[0].friday_to}</p>
-                        <p>---</p>
                         
 
+                        <h2>Schedule</h2>
+                        {person.schedule.map(schdl => {
+                            return(
+                                <div>
+                                    <p>Afedling: {schdl.department}</p>
+                                    <p>Maandag {schdl.monday_from} tot {schdl.monday_to}</p>
+                                    <p>Dinsdag: {schdl.tuesday_from} tot {schdl.tuesday_to}</p>
+                                    <p>Woensdag: {schdl.wednesday_from} tot {schdl.wednesday_to}</p>
+                                    <p>Donderdag: {schdl.thursday_from} tot {schdl.thursday_to}</p>
+                                    <p>Vrijdag: {schdl.friday_from} tot {schdl.friday_to}</p>
+                                </div>
+                            )
+                        })}
+
+
+                        <h2>Aanmeldingen</h2>
+                        {person.registrations.map(item => {
+                           
+                           return(
+                               <div>
+                                <p>Signed in: {item.signed_in}</p>
+                                <p>Signed off: {item.signed_off}</p>
+                                <p>Automatisch: {item.signed_auto_signed_off}</p>
+                                <p>---</p>
+                               </div>
+                           )
+                        })}
                    </div>
                 
             
