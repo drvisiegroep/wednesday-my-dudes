fce05891af9f35c2c9a002e6796f03354bf07cdb
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-25 16:18:43 +0100

    date check ivm null als er geen data is

--- src/Utils/DateHandlers.js
+++ src/Utils/DateHandlers.js
@@ -1,22 +1,18 @@
-import React from "react"
-
 // Datum afronden met substring opties
 export const shortDate = (date, substring1, substring2) => {
-    return date.substring(substring1, substring2)
+    if (date) {
+        return date.substring(substring1, substring2)
+    } else {
+        return <p>Probleem in dateHandler</p>
+    }
 }
 
 // Datum naar naam van de dag in het Nederlands converteren
 export const fullDayNameDutch = (date) => {
-    const regday = new Date(date)
-    return regday.toLocaleDateString("nl-NL", { weekday: 'long' })
+    if (date) {
+        const regday = new Date(date)
+        return regday.toLocaleDateString("nl-NL", { weekday: 'long' })
+    } else {
+        return <p>Probleem in dateHandler</p>
+    }
 }
-
-const DateHandlers = () => {
-    return (
-        <>
-
-        </>
-    )
-}
-
-export default DateHandlers
