329cc71eeccaae7c5efeb25ac4bcc0b213582e34
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-28 18:14:52 +0100

    css import

--- src/Schedule.js
+++ src/Schedule.js
@@ -2,6 +2,8 @@ import React, {useState, } from 'react';
 import Person from './Person';
 import useFetch from './Utils/useFetch';
 import { scheduleUrl } from './Utils/ApiUrls';
+import './css/schedule.css'
+import { shortDate } from './Utils/DateHandlers';
 
 const Schedule = () => {
 
@@ -9,6 +11,9 @@ const Schedule = () => {
     // eslint-disable-next-line no-unused-vars
     const { error, isPending, data: schedule } = useFetch(scheduleUrl())
     
+    // UserID geven we mee als prop naar Person.js
+    const [userid, setUserid] = useState('29');
+
 
     // https://stackoverflow.com/questions/46240647/react-how-to-force-a-function-component-to-render/53837442#53837442
     function useForceUpdate(){
@@ -28,9 +33,6 @@ const Schedule = () => {
     }
 
 
-    // UserID geven we mee als prop naar Person.js
-    const [userid, setUserid] = useState('29');
-
     // Styling 
     const tableWidth = {
         width: "60%",
@@ -45,6 +47,14 @@ const Schedule = () => {
 
     const btnWidth = {
         minWidth: "40px"
+    }
+
+
+    if(isPending) {
+        <p>Loading...</p>
+    }
+    if(error) {
+        <p>Foutje bedankt: {error}</p>
     }
 
     return (
@@ -75,11 +85,11 @@ const Schedule = () => {
                                 <td>{item.user_id}</td>
                                 <td>{item.department}</td>
                                 
-                                {item.monday_from === null ? <td></td> : <td>{item.monday_from.substring(0,5)} - {item.monday_to.substring(0,5)}</td>}
-                                {item.tuesday_from === null ? <td></td> : <td>{item.tuesday_from.substring(0,5)} - {item.tuesday_to.substring(0,5)}</td>}
-                                {item.wednesday_from === null ? <td></td> : <td>{item.wednesday_from.substring(0,5)} - {item.wednesday_to.substring(0,5)}</td>}
-                                {item.thursday_from === null ? <td></td> : <td>{item.thursday_from.substring(0,5)} - {item.thursday_to.substring(0,5)}</td>}
-                                {item.friday_from === null ? <td></td> : <td>{item.friday_from.substring(0,5)} - {item.friday_to.substring(0,5)}</td>}
+                                {item.monday_from === null ? <td></td> : <td>{shortDate(item.monday_from,0,5)} - {shortDate(item.monday_to,0,5)}</td>}
+                                {item.tuesday_from === null ? <td></td> : <td>{shortDate(item.tuesday_from,0,5)} - {shortDate(item.tuesday_to,0,5)}</td>}
+                                {item.wednesday_from === null ? <td></td> : <td>{shortDate(item.wednesday_from,0,5)} - {shortDate(item.wednesday_to,0,5)}</td>}
+                                {item.thursday_from === null ? <td></td> : <td>{shortDate(item.thursday_from,0,5)} - {shortDate(item.thursday_to,0,5)}</td>}
+                                {item.friday_from === null ? <td></td> : <td>{shortDate(item.friday_from,0,5)} - {shortDate(item.friday_to,0,5)}</td>}
 
                             </tr>
                             
