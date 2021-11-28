a6c05516e9aca4f1b7d86bd9ee55eb1e4eea1fd2
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-28 18:13:31 +0100

    button toegevoegd voor timediff test

--- src/Page.js
+++ src/Page.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Schedule from "./Schedule";
 import Presence from "./Presence";
+import Timediff from "./Timediff";
 
 const Page = () => {
 
-    const [resourceType, setResourceType] = useState('schedule')
+    const [resourceType, setResourceType] = useState('timediff')
 
     return (
 
@@ -12,12 +13,14 @@ const Page = () => {
             <div>
                 <button onClick={() => setResourceType('presence')}>Presence</button>
                 <button onClick={() => setResourceType('schedule')}>Schedule</button>
+                <button onClick={() => setResourceType('timediff')}>Timediff</button>
             </div>
             
             <h1>{resourceType}</h1>
 
             {resourceType === 'schedule' && <Schedule  />}
             {resourceType === 'presence' && <Presence  />}
+            {resourceType === 'timediff' && <Timediff  />}
 
         </>
 
