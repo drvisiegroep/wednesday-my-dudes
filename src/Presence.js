c6b4b7112d2c37265b928dfffa214db3ac615320
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2021-11-28 18:14:04 +0100

    load error  check

--- src/Presence.js
+++ src/Presence.js
@@ -1,10 +1,18 @@
 import useFetch from "./Utils/useFetch";
 import { presenceUrl } from './Utils/ApiUrls';
+import { shortDate } from "./Utils/DateHandlers";
 
 const Presence = () => {
 
     // eslint-disable-next-line no-unused-vars
     const { error, isPending, data: presence } = useFetch(presenceUrl())
+
+    if(isPending) {
+        <p>Loading...</p>
+    }
+    if(error) {
+        <p>Foutje bedankt: {error}</p>
+    }
 
     return (
         
@@ -27,7 +35,7 @@ const Presence = () => {
                                     <td>{item.first_name} {item.last_name}</td>
                                     <td>{item.user_id}</td>
                                     <td>{item.department}</td>
-                                    <td>{item.signed_in} - {item.signed_off}</td>
+                                    <td>{shortDate(item.signed_in,11,16)} - {item.signed_off}</td>
                                 </tr>
                             
                             )
